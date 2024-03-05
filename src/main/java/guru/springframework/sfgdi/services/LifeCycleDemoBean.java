115db825c60c3ae0721a24232ed40f43d2b5989e
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2024-03-05 01:03:28 -0500

    merges lifecycledemobean

--- src/main/java/guru/springframework/sfgdi/services/LifeCycleDemoBean.java
+++ src/main/java/guru/springframework/sfgdi/services/LifeCycleDemoBean.java
@@ -1,13 +1,12 @@
 package guru.springframework.sfgdi.services;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 @Component
 public class LifeCycleDemoBean implements InitializingBean, DisposableBean, BeanNameAware,
